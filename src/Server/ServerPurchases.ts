8d9f63f49f6c2cfe378eea312bdb7351435a759a
Sol <Nolshine@users.noreply.github.com>
2024-08-22 01:48:55 +0100

    UI: Fix incorrect information given in pserv upgrade error msg. (#1602)

--- src/Server/ServerPurchases.ts
+++ src/Server/ServerPurchases.ts
@@ -47,7 +47,7 @@ export const getPurchasedServerUpgradeCost = (hostname: string, ram: number): nu
   if (isNaN(ram) || !isPowerOfTwo(ram) || !(Math.sign(ram) === 1))
     throw new Error(`${ram} is not a positive power of 2`);
   if (server.maxRam >= ram)
-    throw new Error(`'${hostname}' current ram (${server.maxRam}) is not bigger than new ram (${ram})`);
+    throw new Error(`The new ram of '${hostname}' (${ram}) must be bigger than its current ram (${server.maxRam}).`);
   return getPurchaseServerCost(ram) - getPurchaseServerCost(server.maxRam);
 };
 
